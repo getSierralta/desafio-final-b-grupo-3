e9bbf5f5ac89e4acea372721e24d1b1e8d3da917
getsierralta <sierraltamaximiliano8@gmail.com>
2021-03-26 15:42:13 +0000

    Initial commit

---
+++ src/main/java/b3/desafiofinal/demo/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package b3.desafiofinal.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = "/")
+    public String index(ModelMap map){
+        map.addAttribute("name", "Max");
+        return "index";
+    }
+}
